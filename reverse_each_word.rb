1562a81dd5223296614ac1208a74202b7a79e95d
Carter Parrett <cparrett94@hotmail.com>
2020-07-09 21:07:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word(string)
-splittedstring = string.split
-arrayofreversedwords = splittedstring.collect do |letter| 
-  letter.reverse 
-  end
-  arrayofreversedwords.join(" ")
-end
+# def reverse_each_word(string)
+# splittedstring = string.split
+# arrayofreversedwords = splittedstring.collect do |letter| 
+#   letter.reverse 
+#   end
+#   arrayofreversedwords.join(" ")
+# end
 
